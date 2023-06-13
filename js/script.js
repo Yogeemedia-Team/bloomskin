ca49a6c967bb4d59b89920326bc2c7eb4166bd2f
manojradeshan <manojradeshan2023@gmail.com>
2023-06-13 09:23:57 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,83 @@
+var countDownDate = new Date("june 15, 2023 00:00:00").getTime();
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hrs = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var secs = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hrs").innerHTML = hrs;
+  document.getElementById("mins").innerHTML = mins;
+  document.getElementById("secs").innerHTML = secs;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("days").innerHTML = 00;
+    document.getElementById("hrs").innerHTML = 00;
+    document.getElementById("mins").innerHTML = 00;
+    document.getElementById("secs").innerHTML = 00;
+  }
+}, 1000);
+
+$(document).ready(function () {
+  $(".counter").each(function () {
+    $(this)
+      .prop("Counter", 0)
+      .animate(
+        {
+          Counter: $(this).text(),
+        },
+        {
+          duration: 4000,
+          easing: "swing",
+          step: function (now) {
+            $(this).text(Math.ceil(now));
+          },
+        }
+      );
+  });
+});
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  autoplay: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 2,
+    },
+  },
+});
+
+    $(document).ready(function () {
+      $(".read-more-btn").click(function () {
+        $(".site-content--default").slideToggle("slow");
+        $(".site-content--default").css({ display: "block" });
+      });
+    });
+ 
+
+    // var acc = document.getElementsByClassName("accordion");
+    // var i;
+
+    // for (i = 0; i < acc.length; i++) {
+    //   acc[i].addEventListener("click", function () {
+    //     this.classList.toggle("active");
+    //     var panel = this.nextElementSibling;
+    //     if (panel.style.display === "block") {
+    //       panel.style.display = "none";
+    //     } else {
+    //       panel.style.display = "block";
+    //     }
+    //   });
+    // }
